c1c36f02ccfe5a77cdffdffaa1e107654058c07c
shai1436 <shahrukh.haider35@gmail.com>
2018-04-11 22:51:50 +0530

    Scaffolding completed

--- server/app.js
+++ server/app.js
@@ -1,25 +1,96 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var index = require('./routes/index');
+var cors = require("cors");
 
 
-const express = require('express');
-const config = require('./config/config');
-const glob = require('glob');
-const mongoose = require('mongoose');
+var auth = require('./routes/auth');
 
-mongoose.connect(config.db);
-const db = mongoose.connection;
-db.on('error', () => {
-  throw new Error('unable to connect to database at ' + config.db);
+var passport = require('passport');
+var session = require('express-session');
+var app = express();
+
+
+
+
+var url = 'mongodb://localhost:27017/login';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
 });
 
-const models = glob.sync(config.root + '/app/models/*.js');
-models.forEach(function (model) {
-  require(model);
+//cross domain stuff
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
 });
-const app = express();
 
-module.exports = require('./config/express')(app, config);
+//configuring passport
+require('./passport')(passport);
 
-app.listen(config.port, () => {
-  console.log('Express server listening on port ' + config.port);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+//console.log("DIRNAME", __dirname);
+//app.use(cors({credentials: true, origin: 'http://localhost:9000'}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+require('./routes/auth.js')(app, passport);
+app.use('/', index);
+
+//setting cookies for cross domain login
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
